2298474a1a1194c0a37b7397d439d21bc265212b
Danilo Cardoso <danilo.cardosolima@gmail.com>
2022-02-07 18:23:09 -0300

    replace css files by styled components

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -1,44 +1,9 @@
 import React, { useState } from "react";
-import { corPrimaria } from "../UI/variaveis.js";
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
-import styled from "styled-components";
-import { Icone } from "../UI/";
+import { Icone, Box, Botao, Saldo, Detalhe } from "../UI/";
 
-const Box = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: white;
-  border-radius: 5px;
-  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.04);
-  padding: 20px;
-  width: 48%;
-`;
-
-const BtnConta = styled.button`
-  margin: 15px auto 0px auto;
-  display: block;
-  border-radius: 20px;
-  background-color: ${corPrimaria};
-  border: none;
-  color: white;
-  font-weight: 600;
-  font-size: 14px;
-  padding: 8px 20px;
-  cursor: pointer;
-`;
-
-const Saldo = styled.div`
-  font-weight: 700;
-  font-size: 32px;
-`;
-
-const Detalhe = styled.span`
-  color: ${corPrimaria};
-  font-size: 24px;
-`;
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
@@ -62,13 +27,13 @@ const Conta = () => {
         ) : null}
       </div>
 
-      <BtnConta onClick={toggleHandler}>
+      <Botao onClick={toggleHandler}>
         <Icone
           marginTop='2px'
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
-      </BtnConta>
+      </Botao>
     </Box>
   );
 };
